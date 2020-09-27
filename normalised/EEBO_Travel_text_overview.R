0a39ea8105fb32779f48fae5507229bf78d5aac4
Stefan Hartmann <hartmann_ste@gmx.de>
2020-09-27 18:08:00 +0200

    improved table

--- normalised/EEBO_Travel_text_overview.R
+++ normalised/EEBO_Travel_text_overview.R
@@ -7,10 +7,10 @@ library(DT)
 # read file
 d <- read_csv("../docs/eebo_travel_selection.csv")
 
-d %>% select(TCP, Author, Date, Length) %>% setNames(c("ID", "Author", "Date", "Tokens")) %>% datatable()
+# d %>% select(TCP, Author, Date, Length) %>% setNames(c("ID", "Author", "Date", "Tokens")) %>% datatable() %>% saveWidget("table.html")
 # d %>% select(TCP, Author, Date, Length) %>% setNames(c("ID", "Author", "Date", "Tokens")) %>% reactable
 
 
 # # select relevant columns
-# d %>% select(TCP, Author, Date, Length) %>% setNames(c("ID", "Author", "Date", "Tokens")) %>% 
-#   kbl() %>% kable_styling(bootstrap_options = c("striped", "hover", "condensed")) %>% scroll_box() %>% save_kable("overview_table.html")
+d %>% select(TCP, Author, Title, Date, Length) %>% setNames(c("ID", "Author", "Title", "Date", "Tokens")) %>% 
+   kbl() %>% kable_styling(bootstrap_options = c("striped", "hover", "condensed")) %>% scroll_box() %>% save_kable("overview_table.html")
